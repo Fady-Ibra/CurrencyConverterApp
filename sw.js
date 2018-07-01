e6c31ed272de3ee88d99c931742f9c72c9bf73e1
Fady Ibrahim <fady.a.m.ibrahim@gmail.com>
2018-07-01 21:57:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+'use strict';
+
 if (!Cache.prototype.add) {
   Cache.prototype.add = function add(request) {
     return this.addAll([request]);
@@ -13,28 +15,23 @@ if (!Cache.prototype.addAll) {
       this.message = message;
     }
     NetworkError.prototype = Object.create(Error.prototype);
-    return Promise.resolve().then(function() {
+    return Promise.resolve().then(function () {
       if (arguments.length < 1) throw new TypeError();
       var sequence = [];
-      requests = requests.map(function(request) {
-        if (request instanceof Request) return request;
-        else return String(request); 
+      requests = requests.map(function (request) {
+        if (request instanceof Request) return request;else return String(request);
       });
-      return Promise.all(
-        requests.map(function(request) {
-          if (typeof request === 'string') request = new Request(request);
-          var scheme = new URL(request.url).protocol;
-          if (scheme !== 'http:' && scheme !== 'https:') throw new NetworkError("Invalid scheme");
-          return fetch(request.clone());
-        })
-      );
-    }).then(function(responses) {
-      return Promise.all(
-        responses.map(function(response, i) {
-          return cache.put(requests[i], response);
-        })
-      );
-    }).then(function() {
+      return Promise.all(requests.map(function (request) {
+        if (typeof request === 'string') request = new Request(request);
+        var scheme = new URL(request.url).protocol;
+        if (scheme !== 'http:' && scheme !== 'https:') throw new NetworkError("Invalid scheme");
+        return fetch(request.clone());
+      }));
+    }).then(function (responses) {
+      return Promise.all(responses.map(function (response, i) {
+        return cache.put(requests[i], response);
+      }));
+    }).then(function () {
       return undefined;
     });
   };
@@ -43,13 +40,13 @@ if (!Cache.prototype.addAll) {
 if (!CacheStorage.prototype.match) {
   CacheStorage.prototype.match = function match(request, opts) {
     var caches = this;
-    return this.keys().then(function(cacheNames) {
+    return this.keys().then(function (cacheNames) {
       var match;
-      return cacheNames.reduce(function(chain, cacheName) {
-        return chain.then(function() {
-          return match || caches.open(cacheName).then(function(cache) {
+      return cacheNames.reduce(function (chain, cacheName) {
+        return chain.then(function () {
+          return match || caches.open(cacheName).then(function (cache) {
             return cache.match(request, opts);
-          }).then(function(response) {
+          }).then(function (response) {
             match = response;
             return match;
           });
@@ -59,30 +56,21 @@ if (!CacheStorage.prototype.match) {
   };
 }
 
-self.addEventListener('install', function(event) {
-  if (self.skipWaiting) { self.skipWaiting(); }//
-  event.waitUntil(
-    caches.open('simple-sw-v1').then(function(cache) {
-      return cache.addAll([
-        './',
-        'jquery.min.js',
-        'jquery-ui.min.js',
-        'jquery-ui.min.css',
-        'db.js',        
-      ]);
-    })
-  );
+self.addEventListener('install', function (event) {
+  if (self.skipWaiting) {
+    self.skipWaiting();
+  } //
+  event.waitUntil(caches.open('simple-sw-v1').then(function (cache) {
+    return cache.addAll(['./', 'jquery.min.js', 'jquery-ui.min.js', 'jquery-ui.min.css', 'db.js']);
+  }));
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request).then(function(response){
-        if(response.status === 404) return new Response(" Whoops !! Not Found ");
-        else return response;
-      }).catch(function(){
-        return new Response(" Whoops !! That is tottaly Failed ");
-      });
-    })
-  );
+self.addEventListener('fetch', function (event) {
+  event.respondWith(caches.match(event.request).then(function (response) {
+    return response || fetch(event.request).then(function (response) {
+      if (response.status === 404) return new Response(" Whoops !! Not Found ");else return response;
+    }).catch(function () {
+      return new Response(" Whoops !! That is tottaly Failed ");
+    });
+  }));
 });
